43f049211079da0e215384d127de76af5479c2c7
andjared <andja.velimirovic@gmail.com>
2023-11-08 11:47:34 +0100

    Add logo to navbar

---
+++ client/src/components/navbar/index.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "../logo";
+
+export const NavBar = () => {
+  return (
+    <nav className="flex items-center justify-between h-[100px] w-full px-8">
+      <Logo />
+    </nav>
+  );
+};
