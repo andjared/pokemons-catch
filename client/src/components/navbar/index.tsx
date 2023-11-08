b3349e53d4a19f22fa01c7089a3e4af350e3ef25
andjared <andja.velimirovic@gmail.com>
2023-11-08 18:26:49 +0100

    Add nav links

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
 import { Logo } from "../logo";
 
 export const NavBar = () => {
   return (
-    <nav className="flex items-center justify-between h-[100px] w-full px-8">
+    <nav className="flex items-center justify-around gap-x-6 h-[100px] w-full px-8">
       <Logo />
+      <div className="flex items-center justify-between gap-x-6">
+        <Link to="/">
+          <span className="uppercase font-medium text-md">Dashboard</span>
+        </Link>
+        <Link to="/">
+          <span className="uppercase font-medium text-md">Pokedex</span>
+        </Link>
+        <Link to="/">
+          <span className="uppercase font-medium text-md">Items</span>
+        </Link>
+        <Link to="/">
+          <span className="uppercase font-medium text-md">Moves</span>
+        </Link>
+      </div>
+      <div>user</div>
     </nav>
   );
 };
