cf50992bc0728135aacf64f825baa61924d5f665
andjared <andja.velimirovic@gmail.com>
2023-11-08 18:11:59 +0100

    Change logo

--- client/src/components/logo/index.tsx
+++ client/src/components/logo/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export const Logo = () => {
   return (
     <Link to="/">
-      <img src="/images/logo.svg" alt="Pokemon logo" />{" "}
+      <img src="/images/logo.svg" alt="Pokemon logo" />
     </Link>
   );
 };
