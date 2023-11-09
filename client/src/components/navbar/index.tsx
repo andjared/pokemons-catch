41087be229c6500b960f7f892bdcd1f0d117c4c9
andjared <andja.velimirovic@gmail.com>
2023-11-09 10:53:09 +0100

    Toggle menu small screens

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import clsx from "clsx";
 import { Logo } from "../logo";
 import { UserMenu } from "../user-menu";
 
 export const NavBar = () => {
+  const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
+
+  const toggleMenu = (): void => {
+    setIsMenuActive(!isMenuActive);
+  };
+
   return (
-    <nav className="flex items-center justify-between  h-[80px] w-full px-6 md:px-8 shadow-md">
+    <nav className="relative flex items-center justify-between  h-[80px] w-full px-6 md:px-8 shadow-md">
       <Logo />
-      <div className="hidden md:flex items-center justify-between gap-x-6">
+      <div
+        className={clsx(
+          isMenuActive
+            ? "absolute top-[84px] left-0 w-full flex flex-col justify-around gap-y-4 p-4 z-10 bg-white"
+            : "hidden"
+        )}
+      >
         <Link to="/">
           <span className="uppercase font-semibold text-sm lg:text-md x">
             Dashboard
@@ -28,7 +42,7 @@ export const NavBar = () => {
           </span>
         </Link>
       </div>
-      <UserMenu />
+      <UserMenu isMenuActive={isMenuActive} toggleMenu={toggleMenu} />
     </nav>
   );
 };
--- client/src/components/user-menu/index.tsx
+++ client/src/components/user-menu/index.tsx
@@ -1,4 +1,9 @@
-export const UserMenu = () => {
+export interface userMenuProps {
+  toggleMenu: () => void;
+  isMenuActive: boolean;
+}
+
+export const UserMenu = ({ toggleMenu, isMenuActive }: userMenuProps) => {
   return (
     <div className="flex justify-center items-center text-grey gap-x-2">
       <img
@@ -6,7 +11,13 @@ export const UserMenu = () => {
         alt="Avatar image"
         className="rounded-full bg-gray-50"
       />
-      <img src="/icons/menu.svg" alt="Arrow down icon" />
+      <button onClick={toggleMenu} className="md:hidden">
+        {isMenuActive ? (
+          <img src="/icons/close.svg" alt="Arrow down icon" />
+        ) : (
+          <img src="/icons/menu.svg" alt="Arrow down icon" />
+        )}
+      </button>
     </div>
   );
 };
