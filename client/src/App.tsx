ed2c7b4915c010e937901b9a25009f81a43a3cdd
andjared <andja.velimirovic@gmail.com>
2023-11-13 12:21:54 +0100

    Fetch pokemon data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,41 @@ import {
   RouterProvider,
   Outlet,
 } from "react-router-dom";
+import axios from "axios";
 import { NavBar } from "./components/navbar";
 import { Dashboard } from "./pages/dashboard";
 import { Pokedex } from "./pages/pokedex";
 import { Items } from "./pages/items";
 import { Moves } from "./pages/moves";
+import { useQuery } from "react-query";
 
 function App() {
+  async function getPokemonData(pokemons: any) {
+    const promises = pokemons.map((pokemon: any) => axios.get(pokemon.url));
+    return Promise.all(promises);
+  }
+
+  async function getPokemonsList() {
+    const { data } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?offset=50&limit=20`
+    );
+    const response = data.results;
+    return response;
+  }
+
+  const { data: list } = useQuery({
+    queryKey: ["pokemon list"],
+    queryFn: getPokemonsList,
+  });
+
+  const { data: pokemons } = useQuery({
+    queryKey: ["pokemons data"],
+    queryFn: () => getPokemonData(list),
+    enabled: !!list,
+  });
+
+  if (pokemons) pokemons.map((pokemon) => console.log(pokemon.data));
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
